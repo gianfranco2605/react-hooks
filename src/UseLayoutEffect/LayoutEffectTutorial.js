f294f791dc7e218588a5b48063bee11c2504dc64
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-01-19 09:09:16 +0100

    useLayoutEffect

--- src/UseLayoutEffect/LayoutEffectTutorial.js
+++ src/UseLayoutEffect/LayoutEffectTutorial.js
@@ -3,10 +3,11 @@ import { useLayoutEffect, useEffect, useRef } from "react";
 function LayoutEffectTutorial() {
   const inputRef = useRef(null);
 
+  // Layout always calls before the useEffect or the page is rendered
   useLayoutEffect(() => {
     console.log(inputRef.current.value);
   }, []);
-
+  //useEffect is call after the page is rendered
   useEffect(() => {
     inputRef.current.value = "HELLO";
   }, []);
